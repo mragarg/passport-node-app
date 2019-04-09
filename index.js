130fe7cbfe0664aa820ef9c3052b5f580f8fdd2a
Ashish Garg <mragarg@gmail.com>
2019-04-09 09:55:33 -0400

    Get handler

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1> Hello </h1>
+    `)
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`You are running on port ${process.env.PORT}`);
+});
